5be8c7dca9b402aba0f0ef088d0aaa558bafa131
idigan <fz98712@gmail.com>
2024-02-29 08:18:56 +0800

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [
+    {
+      name: "test", // 替换成你自己的任意名称
+      script: "main.js", // 替换成你自己的项目入口文件
+      env: {
+        COMMON_VARIABLE: "true",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "root", // 替换成你自己的主机名
+      host: "8.139.6.25", // 替换成你自己的主机 ip
+      port: "9000", // 替换成你自己的端口号
+      ref: "origin/main", // 替换成你自己的分支
+      repo: "git@github.com:idigan/share-footerball.git", // 替换成你自己的仓库地址
+      ssh_options: "StrictHostKeyChecking=no",
+      path: "/test", // 替换成你自己要在服务器上部署的目录
+      "pre-deploy": "git fetch --all",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+    },
+  },
+};
