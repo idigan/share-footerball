dbc5b5ee25071a6a86e21c8bf8501c17723944fc
idigan <fz98712@gmail.com>
2024-02-29 20:20:22 +0800

    更改

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,13 +15,13 @@ module.exports = {
   deploy: {
     production: {
       // user: "iZbp1i5bth3hh7nnscm8maZ", // 替换成你自己的主机名
-      user: "idigan", // 替换成你自己的主机名
+      user: "张云杰", // 替换成你自己的主机名
       host: "8.139.6.25", // 替换成你自己的主机 ip
       port: "9000", // 替换成你自己的端口号
       ref: "origin/main", // 替换成你自己的分支
       repo: "git@github.com:idigan/share-footerball.git", // 替换成你自己的仓库地址
       ssh_options: "StrictHostKeyChecking=no",
-      path: "/test", // 替换成你自己要在服务器上部署的目录
+      path: "/root/test", // 替换成你自己要在服务器上部署的目录
       "pre-deploy": "git fetch --all",
       "post-deploy":
         "npm install && pm2 reload ecosystem.config.js --env production",
